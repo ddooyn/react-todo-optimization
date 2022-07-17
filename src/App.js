d01cdac6badc202398b71f1864498026606afae2
Sooyoung Yoon <ayoooo240@gmail.com>
2022-07-17 16:34:32 +0900

    ⚡️ React.memo, useState의 함수형 업데이트 기능으로 컴포넌트 최적화

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function createBulkTodos() {
       id: i,
       text: `할 일 ${i}`,
       checked: false,
-    })
+    });
   }
   return array;
 }
@@ -19,36 +19,27 @@ const App = () => {
   const [todos, setTodos] = useState(createBulkTodos);
 
   const nextId = useRef(2501);
-  const onInsert = useCallback(
-    (text) => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1;
-    },
-    [todos],
-  );
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    setTodos((todos) => todos.concat(todo));
+    nextId.current += 1;
+  }, []);
 
-  const onRemove = useCallback(
-    (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
-    },
-    [todos],
-  );
+  const onRemove = useCallback((id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }, []);
 
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },
-    [todos],
-  );
+  const onToggle = useCallback((id) => {
+    setTodos((todos) =>
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      ),
+    );
+  }, []);
 
   return (
     <TodoTemplate>
