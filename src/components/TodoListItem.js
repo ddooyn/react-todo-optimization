ecae3c813a1cf6906fff17ca04292e7abf90c250
Sooyoung Yoon <ayoooo240@gmail.com>
2022-07-17 15:15:27 +0900

    ✨ Todo 항목 삭제 기능 구현

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,8 +7,8 @@ import {
 } from 'react-icons/md';
 import cn from 'classnames';
 
-const TodoListItem = ({ todo }) => {
-  const { text, checked } = todo;
+const TodoListItem = ({ todo, onRemove }) => {
+  const { id, text, checked } = todo;
 
   return (
     <div className="TodoListItem">
@@ -16,7 +16,7 @@ const TodoListItem = ({ todo }) => {
         {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
         <div className="text">{text}</div>
       </div>
-      <div className="remove">
+      <div className="remove" onClick={() => onRemove(id)}>
         <MdRemoveCircleOutline />
       </div>
     </div>
