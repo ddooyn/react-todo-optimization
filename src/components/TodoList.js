61c19b2463c8741862fb0ad3342b06bc1e868f4a
Sooyoung Yoon <ayoooo240@gmail.com>
2022-07-17 17:23:33 +0900

    ⚡️ TodoList 컴포넌트 React.memo로 최적화

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,10 +6,15 @@ const TodoList = ({ todos, onRemove, onToggle }) => {
   return (
     <div className="TodoList">
       {todos.map((todo) => (
-        <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} />
+        <TodoListItem
+          todo={todo}
+          key={todo.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
