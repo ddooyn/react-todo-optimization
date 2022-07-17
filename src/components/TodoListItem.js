bf8c7400fdbea9c111f8d3db7651723fd351a64b
Sooyoung Yoon <ayoooo240@gmail.com>
2022-07-17 15:42:20 +0900

    ✨ Todo 항목 수정(체크토글) 기능 구현

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,12 +7,12 @@ import {
 } from 'react-icons/md';
 import cn from 'classnames';
 
-const TodoListItem = ({ todo, onRemove }) => {
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
   const { id, text, checked } = todo;
 
   return (
     <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })}>
+      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
         {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
         <div className="text">{text}</div>
       </div>
