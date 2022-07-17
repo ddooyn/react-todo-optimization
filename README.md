38aa20c5c78dd5734346a5e8cbc90dddfcf29e33
Sooyoung Yoon <ayoooo240@gmail.com>
2022-07-17 14:46:17 +0900

    ✨ TodoInsert value 상태 관리 📝 리드미 추가 (useCallback)

--- README.md
+++ README.md
@@ -95,3 +95,18 @@ const TodoListItem = ({ todo }) => {
   }
 }
 ```
+
+---
+> ## useCallback(() => {}, [])
+컴포넌트가 리렌더링될 때마다 함수를 새로 만드는 것이 아니라,   
+한번 함수를 만든 뒤 재사용할 수 있도록 `useCallback` Hook 이용
+```jsx
+const TodoInsert = () => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+  // ...
+}
+```
--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import './TodoInsert.scss';
 import { MdAdd } from 'react-icons/md';
 
 const TodoInsert = () => {
+  const [value, setValue] = useState('');
+
+  // 컴포넌트가 리렌더링될 때마다 함수를 새로 만드는 것이 아니라,
+  // 한번 함수를 만든 뒤 재사용할 수 있도록 useCallback
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
   return (
     <form className="TodoInsert">
-      <input type="text" placeholder="할 일을 입력하세요" />
+      <input
+        type="text"
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button>
         <MdAdd />
       </button>
