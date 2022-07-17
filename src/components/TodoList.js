b6d172d6f6a09f0279cd9d664aefe12767568b01
Sooyoung Yoon <ayoooo240@gmail.com>
2022-07-17 18:15:12 +0900

    ⚡️ react-virtualized를 활용한 리스트 렌더링 최적화

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import './TodoList.scss';
 import TodoListItem from './TodoListItem';
+import { List } from 'react-virtualized';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
-  return (
-    <div className="TodoList">
-      {todos.map((todo) => (
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
         <TodoListItem
           todo={todo}
-          key={todo.id}
+          key={key}
           onRemove={onRemove}
           onToggle={onToggle}
+          style={style}
         />
-      ))}
+      );
+    },
+    [onRemove, onToggle, todos],
+  );
+
+  return (
+    <div className="TodoList">
+      <List
+        className="TodoList"
+        width={512}
+        height={513}
+        rowCount={todos.length}
+        rowHeight={57}
+        rowRenderer={rowRenderer}
+        list={todos}
+        style={{ outline: 'none' }}
+      />
     </div>
   );
 };
